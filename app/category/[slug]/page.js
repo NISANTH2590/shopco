8ae4ea1a83b54805ccd9b160131154ee676125a1
Nisanth <nisanth2590@gmail.com>
2024-09-25 17:10:56 +0530

    second commit

--- app/category/[slug]/page.js
+++ app/category/[slug]/page.js
@@ -5,10 +5,9 @@ import styles from '@/styles/pages/category-page.module.scss'
 const CategoryPage = () => {
     return (
         <div className={styles.categoryPageLayout}>
-            <BreadCrumbs />
-            This is the category page
+                        This is the category page
         </div>
-    );
+    );    
 }
 
 export default CategoryPage;
